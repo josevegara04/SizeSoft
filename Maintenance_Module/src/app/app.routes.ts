2d188aff80e258dd8d57933ccea1a5c6b048ae5f
josevegara04 <jbvegar@eafit.edu.co>
2026-02-22 17:34:38 -0500

    submenus created

--- Maintenance_Module/src/app/app.routes.ts
+++ Maintenance_Module/src/app/app.routes.ts
@@ -1,16 +1,26 @@
 import { Routes } from '@angular/router';
 import { MantenimientoLayoutComponent } from './mantenimiento/mantenimiento-layout';
 import { MaestrosComponent } from './mantenimiento/maestros/maestros';
+import { MaquinasEquiposLocalidadesComponent } from './mantenimiento/maestros/maquinas-equipos-localidades/maquinas-equipos-localidades';
+import { RepuestosComponent } from './mantenimiento/maestros/repuestos/repuestos';
 import { TransaccionesComponent } from './mantenimiento/transacciones/transacciones';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'mantenimiento/maestros' },
+  { path: '', pathMatch: 'full', redirectTo: 'mantenimiento/maestros/maquinas-equipos-localidades' },
   {
     path: 'mantenimiento',
     component: MantenimientoLayoutComponent,
     children: [
-      { path: '', pathMatch: 'full', redirectTo: 'maestros' },
-      { path: 'maestros', component: MaestrosComponent },
+      { path: '', pathMatch: 'full', redirectTo: 'maestros/maquinas-equipos-localidades' },
+      {
+        path: 'maestros',
+        component: MaestrosComponent,
+        children: [
+          { path: '', pathMatch: 'full', redirectTo: 'maquinas-equipos-localidades' },
+          { path: 'maquinas-equipos-localidades', component: MaquinasEquiposLocalidadesComponent },
+          { path: 'repuestos', component: RepuestosComponent },
+        ],
+      },
       { path: 'transacciones', component: TransaccionesComponent },
     ],
   },

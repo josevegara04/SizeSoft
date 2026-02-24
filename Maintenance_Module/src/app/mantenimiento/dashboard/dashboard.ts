9d80d039b39f6ffcdde9c7c1f2d5a6c56054ca52
josevegara04 <jbvegar@eafit.edu.co>
2026-02-24 15:58:27 -0500

    <feat>: added dashboard interface

--- Maintenance_Module/src/app/mantenimiento/dashboard/dashboard.ts
+++ Maintenance_Module/src/app/mantenimiento/dashboard/dashboard.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './dashboard.html',
+  styleUrl: './dashboard.css',
 })
 export class DashboardComponent {}

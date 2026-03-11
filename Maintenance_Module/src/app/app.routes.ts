877191cf872dd10d83c4f400fe7d1ccc22831220
josevegara04 <jbvegar@eafit.edu.co>
2026-03-11 17:17:16 -0500

    fix

--- Maintenance_Module/src/app/app.routes.ts
+++ Maintenance_Module/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { MantenimientoLayoutComponent } from './mantenimiento/mantenimiento-layout';
-import { DashboardComponent } from './mantenimiento/dashboard/dashboard'; 
 import { MaestrosComponent } from './mantenimiento/maestros/maestros';
 import { ActividadesMantenimientoComponent } from './mantenimiento/maestros/actividades-mantenimiento/actividades-mantenimiento';
 import { CausasMantenimientoComponent } from './mantenimiento/maestros/causas-mantenimiento/causas-mantenimiento';
@@ -22,12 +21,6 @@ export const routes: Routes = [
     component: MantenimientoLayoutComponent,
     children: [
       { path: '', pathMatch: 'full', redirectTo: 'maestros/maquinas-equipos-localidades' },
-      {
-        path: 'dashboard',
-        component: DashboardComponent,
-        children: [
-        ],
-      },
       {
         path: 'maestros',
         component: MaestrosComponent,

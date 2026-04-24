040a69ff366ff5625838ab25a3d9e63e9a66a105
josevegara04 <jbvegar@eafit.edu.co>
2026-04-24 10:02:48 -0500

    feat: create orde-servicio components

---
+++ Maintenance_Module/src/app/mantenimiento/transacciones/ordenes-servicio/orden-servicio.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ordenes-servicio',
+  standalone: true,
+  templateUrl: "./orden-servicio.component.html" ,
+  styleUrl: "./orden-servicio.component.css",
+})
+export class OrdenesServicioComponent {}

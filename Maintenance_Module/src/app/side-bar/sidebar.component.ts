ed0bcd1affaae9fa4af68a178704175e1746223c
josevegara04 <jbvegar@eafit.edu.co>
2026-04-24 11:53:59 -0500

    feat: add logs to the sidebar for creation of maintenance orders

--- Maintenance_Module/src/app/side-bar/sidebar.component.ts
+++ Maintenance_Module/src/app/side-bar/sidebar.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
+import { SidebarService } from './sidebar.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrls: ['./sidebar.component.css'],
+  imports: [CommonModule],
 })
 export class SidebarComponent {
 
-  logs: string[] = [];
+  constructor(
+    public sidebarService: SidebarService,
+    private cdr: ChangeDetectorRef
+  ) {}
 
+  // look for changes
+  ngDoCheck() {
+    this.cdr.detectChanges();
+  }
 }

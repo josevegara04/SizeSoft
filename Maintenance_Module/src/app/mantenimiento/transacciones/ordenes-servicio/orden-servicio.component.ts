81bcbbb574105e39b4e5b1bb4a12e11fa57da309
josevegara04 <jbvegar@eafit.edu.co>
2026-04-24 11:26:49 -0500

    feat: add api call functionallity

--- Maintenance_Module/src/app/mantenimiento/transacciones/ordenes-servicio/orden-servicio.component.ts
+++ Maintenance_Module/src/app/mantenimiento/transacciones/ordenes-servicio/orden-servicio.component.ts
@@ -1,9 +1,68 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { OrdenServicioService } from './orden-servicio.service';
+import { ApiService } from '../../../services/api.service';
 
 @Component({
   selector: 'app-ordenes-servicio',
   standalone: true,
   templateUrl: "./orden-servicio.component.html" ,
-  styleUrl: "./orden-servicio.component.css",
+  styleUrls: ["./orden-servicio.component.css"],
+  imports: [FormsModule],
 })
-export class OrdenesServicioComponent {}
+export class OrdenesServicioComponent {
+
+  // constructor from orden-servicio.service
+  constructor(
+    private ordenService: OrdenServicioService,
+    private apiService: ApiService
+  ) {
+    console.log('COMPONENTE CARGADO');
+  }
+  // main variables
+  codiOrdMaqu: string = '';
+  codiMaqu: string = '';
+  fechaInicio: string = '';
+  tipoMant: string = '';
+  idMantenimiento: number | null = null;
+  idRepuesto: number | null = null;
+  cantidad: number | null = null;
+
+  // create order
+  createOrder() {
+    console.log('CLICK FUNCIONA');
+
+    const body = [
+      {
+        CodiOrdMaqu: this.codiOrdMaqu,
+        CodiMaqu: this.codiMaqu,
+        Fecha_inicio: new Date(this.fechaInicio).toISOString(),
+        TipoMant: this.tipoMant,
+        idMantenimiento: this.idMantenimiento,
+        fechaFin: null,
+        idRepues: this.idRepuesto,
+        cantid: this.cantidad,
+  
+        // 🔥 dinámicos desde ApiService
+        CodiComp: this.apiService.clsUser.CodiComp,
+        Token: this.apiService.lstrToken,
+  
+        // 🔥 fijo (según backend)
+        Entidad: 303,
+        Accion: 1
+      }
+    ];
+  
+    console.log('Body enviado:', body);
+  
+    this.ordenService.saveOrden(body).subscribe({
+      next: (res) => {
+        console.log('Respuesta backend:', res);
+      },
+      error: (err) => {
+        console.error('Error:', err);
+      }
+    });
+  
+  }
+}

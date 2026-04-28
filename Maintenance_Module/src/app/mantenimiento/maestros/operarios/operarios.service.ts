3df202c1cb47b04062add7f4e38ac3a1234c5d3a
ssantamars <ssantamars@eafit.edu.co>
2026-04-28 12:35:27 -0500

    Gestión de operarios con conexión a API

--- Maintenance_Module/src/app/mantenimiento/maestros/operarios/operarios.service.ts
+++ Maintenance_Module/src/app/mantenimiento/maestros/operarios/operarios.service.ts
@@ -1,24 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { ApiService } from '../../../services/api.service';
 
 @Injectable({ providedIn: 'root' })
 export class OperariosService {
 
-  constructor(private apiService: ApiService) {}
+  private API_URL       = 'https://erpapipruebas.azurewebsites.net/api/Query/Save';
+  private API_URL_QUERY = 'https://erpapipruebas.azurewebsites.net/api/Query';
 
-  /** Accion 1 = insert/update | Accion 2 = delete */
-  save(body: any): Observable<any> {
-    return this.apiService.SaveEntity([{
-      ...body,
-      CodiComp: this.apiService.clsUser.CodiComp,
-      Token:    this.apiService.lstrToken,
-      Entidad:  304,
-    }]);
+  constructor(private http: HttpClient) {}
+
+  save(body: any[]): Observable<any> {
+    return this.http.post(this.API_URL, body);
   }
 
   search(body: any[]): Observable<any> {
-    this.apiService.clsQuery = body;
-    return this.apiService.getQuery();
+    return this.http.post(this.API_URL_QUERY, body);
   }
 }
